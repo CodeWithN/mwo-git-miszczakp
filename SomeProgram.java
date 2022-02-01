e4c52d2555d135b0da77261f8b3782b0aa7a43d3
CodeWithN <76015639+CodeWithN@users.noreply.github.com>
2022-02-01 17:37:00 +0100

    Update SomeProgram.java

--- SomeProgram.java
+++ SomeProgram.java
@@ -3,8 +3,8 @@
 public class SomeProgram { 
     public static void main(String[] args) {
       System.out.println("We are learning to use Git."); 
-System.out.println("We will be Git Experts, soon.");
-
+      System.out.println("We will be Git Experts, soon.");
+      System.out.println("New extra line.");
     }
 
 public void manageTheSatelliteBranch2() {
